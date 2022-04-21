055f193205c726f1638a41a2b2fc960d1587489b
DEDI AHYAR <103528175+dediahyard0219317@users.noreply.github.com>
2022-04-21 08:11:00 -0700

    Add files via upload

---
+++ image class.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DEDI AHYAR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DEDI AHYAR_D0219317'),
+      ),
+      body: Center(
+        child: Image(image: AssetImage('assets/profile.png')),
+      ),
+    );
+  }
+}
